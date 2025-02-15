81dccd411253c07e8ca6fe0f9aaa414cb1fbaad2
FC (Fay) Stegerman <flx@obfusk.net>
2025-02-15 02:54:58 +0100

    WIP

--- apkrepotool/hooks/extract-icons.py
+++ apkrepotool/hooks/extract-icons.py
@@ -6,17 +6,13 @@
 from __future__ import annotations
 
 import argparse
-import io
 import logging
-import re
-import xml.etree.ElementTree as ET
 import zipfile
 
 from pathlib import Path
-from typing import Any, Dict, Optional, Tuple
+from typing import Dict, Optional, Tuple
 
 import apkrepotool
-import repro_apk.binres as binres
 
 
 def process_apks(tc: apkrepotool.ToolConfig, *,
@@ -56,12 +52,12 @@ def extract_icon(apkfile: Path, manifest: apkrepotool.Manifest, icon_path: Path,
     log = logging.getLogger(__name__)
     with zipfile.ZipFile(apkfile) as zf:
         infos = {i.orig_filename: i for i in zf.infolist()}
-        # for png_icon in (manifest.png_icons or []):
-        #     if png_icon not in infos:
-        #         log.warning(f"Missing {png_icon!r} from {str(apkfile)!r}.")
-        #         continue
-        #     if extract_png_from_apk(apkfile, zf, infos[png_icon], icon_path):
-        #         return png_icon
+        for png_icon in (manifest.png_icons or []):
+            if png_icon not in infos:
+                log.warning(f"Missing {png_icon!r} from {str(apkfile)!r}.")
+                continue
+            if extract_png_from_apk(apkfile, zf, infos[png_icon], icon_path):
+                return png_icon
         if try_webp:
             for webp_icon in (manifest.webp_icons or []):
                 if webp_icon not in infos:
@@ -136,172 +132,22 @@ def convert_xml_from_apk(apkfile: Path, zf: zipfile.ZipFile, info: zipfile.ZipIn
 
     Requires Pillow and CairoSVG.
     """
-    assert apkfile
-    if data := parse_xml_icon(zf, info):
-        icon_path.parent.mkdir(parents=True, exist_ok=True)
-        with icon_path.open("wb") as fh:
-            fh.write(data)
-        return True
-    return None if data is None else False
-
-
-# FIXME
-def parse_xml_icon(zf: zipfile.ZipFile, info: zipfile.ZipInfo) -> Optional[bytes]:
     log = logging.getLogger(__name__)
     try:
-        import PIL.Image        # noqa: F401
+        import apkrepotool.xml_icons as xml_icons
+        import repro_apk.binres as binres
     except ImportError:
-        log.warning("Unable to import Pillow.")
+        log.warning("Unable to import xml_icons.")
         return None
     try:
-        import cairosvg         # type: ignore[import-untyped]  # noqa: F401
-    except ImportError:
-        log.warning("Unable to import CairoSVG.")
-        return None
-    infos = {i.orig_filename: i for i in zf.infolist()}     # FIXME
-    resources = binres.read_chunk(zf.read(infos[binres.ARSC_FILE]))[0] if binres.ARSC_FILE in infos else None
-    if resources is not None and not isinstance(resources, binres.ResourceTableChunk):
-        log.warning("Unable to parse AXML.")
-        return b""
-    return _parse_xml_icon(zf, info, resources) or b""
-
-
-# FIXME
-def _parse_xml_icon(zf: zipfile.ZipFile, info: zipfile.ZipInfo,
-                    resources: Optional[binres.ResourceTableChunk]) -> Optional[bytes]:
-    import cairosvg
-    log = logging.getLogger(__name__)
-    axml_chunk = binres.read_chunk(zf.read(info))[0]
-    if not isinstance(axml_chunk, binres.XMLChunk):
-        log.warning("Unable to parse AXML.")
-        return None
-    xml_root = binres.xmlchunk_to_etree(axml_chunk, resources=resources).getroot()
-    if xml_root.tag == "adaptive-icon":
-        bg, fg = xml_root.find("background"), xml_root.find("foreground")
-        if bg is None or fg is None:
-            return None
-        bg_d, fg_d = bg.get(_android("drawable")), fg.get(_android("drawable"))
-        if bg_d is None or fg_d is None:
-            return None
-        bg_img = _load_drawable(zf, bg_d, resources)
-        fg_img = _load_drawable(zf, fg_d, resources)
-        if bg_img is None or fg_img is None:
-            return None
-        if bg_img.size > fg_img.size:
-            bg_img = bg_img.resize(fg_img.size)
-        elif fg_img.size > bg_img.size:
-            fg_img = fg_img.resize(bg_img.size)
-        bg_img.alpha_composite(fg_img)
-        bg_img.show()
-        return b""
-        # bio = io.BytesIO()
-        # bg_img.save(bio, "PNG")
-        # return bio.getvalue()
-    if xml_root.tag == "vector":
-        if tree := _vector_to_svg(xml_root):
-            bio = io.BytesIO()
-            tree.write(bio)
-            print(bio.getvalue())
-            data = cairosvg.svg2png(bytestring=bio.getvalue(), output_width=512, output_height=512)
-            assert isinstance(data, bytes)
-            return data
-        return b""
-    log.warning(f"Unsupported tag: {xml_root.tag!r}.")
-    return None
-
-
-# FIXME
-def _load_drawable(zf: zipfile.ZipFile, drawable: str,
-                   resources: Optional[binres.ResourceTableChunk]) -> Optional[Any]:
-    import PIL.Image
-    infos = {i.orig_filename: i for i in zf.infolist()}     # FIXME
-    if re.fullmatch("#[0-9a-f]{8}", drawable):
-        return PIL.Image.new("RGBA", (512, 512), _rgba(drawable))
-    if drawable.endswith(".xml"):
-        if data := _parse_xml_icon(zf, infos[drawable], resources):
-            bio = io.BytesIO(data)
-        else:
-            return None
-    elif drawable.endswith(".png"):
-        bio = io.BytesIO(zf.read(infos[drawable]))
-    else:
-        return None
-    # FIXME: handle errors
-    with PIL.Image.open(bio, formats=["PNG"]) as im:
-        im.load()
-        return im
-    return None
-
-
-# FIXME
-def _vector_to_svg(xml_root: ET.Element) -> Optional[ET.ElementTree]:
-    attrs = xml_root.attrib.copy()
-    _ = attrs.pop(_android("width"), None)      # FIXME
-    _ = attrs.pop(_android("height"), None)     # FIXME
-    vp_width = attrs.pop(_android("viewportWidth"), None)
-    vp_height = attrs.pop(_android("viewportHeight"), None)
-    if vp_width is None or vp_height is None or attrs:
-        return None
-    viewbox = f"0 0 {vp_width} {vp_height}"
-    svg = ET.Element("svg", {"xmlns": "http://www.w3.org/2000/svg", "viewBox": viewbox})
-    for xml_elem in xml_root:
-        if not _process_vector_elem(svg, xml_elem):
-            return None
-    return ET.ElementTree(svg)
-
-
-# FIXME
-def _process_vector_elem(svg_elem: ET.Element, xml_elem: ET.Element) -> bool:
-    attrs = xml_elem.attrib.copy()
-    if xml_elem.tag == "group":
-        # FIXME: rotate, ...
-        scale_x = attrs.pop(_android("scaleX"), "1")
-        scale_y = attrs.pop(_android("scaleY"), "1")
-        trans_x = attrs.pop(_android("translateX"), "0")
-        trans_y = attrs.pop(_android("translateY"), "0")
-        # FIXME: why reversed?
-        transform = f"translate({trans_x}, {trans_y}) scale({scale_x}, {scale_y})"
-        svg_subelem = ET.SubElement(svg_elem, "g", {"transform": transform})
-        for xml_subelem in xml_elem:
-            if not _process_vector_elem(svg_subelem, xml_subelem):
-                return False
-    elif xml_elem.tag == "path":
-        # FIXME: check colours
-        cap = dict(enumerate(("butt", "round", "square")))
-        join = dict(enumerate(("miter", "round", "bevel")))
-        data = attrs.pop(_android("pathData"), "")
-        fill = _rgba(attrs.pop(_android("fillColor"), "none"))
-        stroke = _rgba(attrs.pop(_android("strokeColor"), "none"))
-        stroke_w = attrs.pop(_android("strokeWidth"), "0")
-        stroke_a = attrs.pop(_android("strokeAlpha"), "1")
-        fill_a = attrs.pop(_android("fillAlpha"), "1")
-        stroke_lc = attrs.pop(_android("strokeLineCap"), "0")
-        stroke_lj = attrs.pop(_android("strokeLineJoin"), "0")
-        fill_t = attrs.pop(_android("fillType"), "nonZero")
-        fill = "none" if fill == "#00000000" else fill
-        svg_subelem = ET.SubElement(svg_elem, "path", {
-            "d": data, "fill": fill, "stroke": stroke, "stroke-width": stroke_w,
-            "stroke-opacity": stroke_a, "fill-opacity": fill_a,
-            "stroke-linecap": cap[int(stroke_lc)],
-            "stroke-linejoin": join[int(stroke_lj)],
-            "fill-rule": fill_t.lower(),
-        })
-    else:
+        data = xml_icons.extract_icon(zf, info.orig_filename)
+    except (xml_icons.Error, binres.Error) as e:
+        log.warning(f"Unable to use {info.orig_filename!r} from {str(apkfile)!r}: {e}.")
         return False
-    if attrs:
-        return False
+    icon_path.parent.mkdir(parents=True, exist_ok=True)
+    with icon_path.open("wb") as fh:
+        fh.write(data)
     return True
-
-
-# FIXME
-def _rgba(colour: str) -> str:
-    if re.fullmatch("#[0-9a-f]{8}", colour):
-        return f"#{colour[3:]}{colour[1:3]}"
-    return colour
-
-
-def _android(attr: str) -> str:
-    return f"{{{binres.SCHEMA_ANDROID}}}{attr}"
 
 
 # FIXME: how to properly handle errors?
---
+++ apkrepotool/xml_icons.py
@@ -0,0 +1,232 @@
+#!/usr/bin/python3
+# encoding: utf-8
+# SPDX-FileCopyrightText: 2025 FC (Fay) Stegerman <flx@obfusk.net>
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from __future__ import annotations
+
+import io
+import re
+import xml.etree.ElementTree as ET
+import zipfile
+
+from typing import Dict, List, Optional, Tuple
+
+import cairosvg                 # type: ignore[import-untyped]
+import PIL.Image
+
+import repro_apk.binres as binres
+
+SCHEMA_SVG = "http://www.w3.org/2000/svg"
+DRAWABLE = f"{{{binres.SCHEMA_ANDROID}}}drawable"
+ARGB = re.compile(r"\A#[0-9a-f]{8}\Z")
+CAP = dict(enumerate(("butt", "round", "square")))
+JOIN = dict(enumerate(("miter", "round", "bevel")))
+FILL = dict(enumerate(("evenodd", "nonzero")))
+TAGMAP = {"vector": "svg", "group": "g", "path": "path"}
+
+
+class Error(Exception):
+    """Base class for errors."""
+
+
+def extract_icon(zf: zipfile.ZipFile, filename: str, *, size: int = 512) -> bytes:
+    """Extract XML icon by converting to SVG and then to PNG data."""
+    infos = {i.orig_filename: i for i in zf.infolist()}
+    resources = binres.read_chunk(zf.read(infos[binres.ARSC_FILE]))[0] if binres.ARSC_FILE in infos else None
+    if resources is not None and not isinstance(resources, binres.ResourceTableChunk):
+        raise Error("Unable to parse AXML")
+    return _extract_icon(zf, infos, filename, resources=resources, size=size)
+
+
+# FIXME: check end tags?
+def _extract_icon(zf: zipfile.ZipFile, infos: Dict[str, zipfile.ZipInfo], filename: str,
+                  *, resources: Optional[binres.ResourceTableChunk], size: int) -> bytes:
+    axml_chunk = binres.read_chunk(zf.read(infos[filename]))[0]
+    if not isinstance(axml_chunk, binres.XMLChunk):
+        raise Error("Unable to parse AXML")
+    adaptive_icon = background = foreground = bg_drawable = fg_drawable = None
+    for i, c in enumerate(axml_chunk.children):
+        if isinstance(c, binres.XMLElemStartChunk):
+            if c.name == "vector":
+                return _extract_vector(axml_chunk.children[i:], size=size)
+            if c.name == "adaptive-icon":
+                if adaptive_icon:
+                    raise Error("Duplicate <adaptive-icon>")
+                adaptive_icon = c
+            elif not adaptive_icon:
+                raise Error("Expected <adaptive-icon> or <vector>")
+            elif c.name == "background":
+                if background:
+                    raise Error("Duplicate <background>")
+                background = c
+                if not (a := c.attrs_as_dict.get(DRAWABLE)):
+                    raise Error("Missing drawable attr for <background>")
+                bg_drawable = binres.brv_str_deref(a.typed_value, a.raw_value, resources=resources)
+            elif c.name == "foreground":
+                if foreground:
+                    raise Error("Duplicate <foreground>")
+                foreground = c
+                if not (a := c.attrs_as_dict.get(DRAWABLE)):
+                    raise Error("Missing drawable attr for <foreground>")
+                fg_drawable = binres.brv_str_deref(a.typed_value, a.raw_value, resources=resources)
+            elif c.name == "monochrome":
+                pass
+            else:
+                raise Error(f"Unsupported tag: {c.name!r}")
+            if background and foreground:
+                break
+    if not (bg_drawable and fg_drawable):
+        raise Error("Missing <background> and/or <foreground>")
+    return _combine_images(
+        _extract_drawable(zf, infos, bg_drawable, resources=resources, size=size),
+        _extract_drawable(zf, infos, fg_drawable, resources=resources, size=size))
+
+
+def _combine_images(bg: PIL.Image.Image, fg: PIL.Image.Image) -> bytes:
+    if bg.mode != "RGBA":
+        bg = bg.convert("RGBA")
+    if fg.mode != "RGBA":
+        fg = fg.convert("RGBA")
+    if bg.size > fg.size:
+        bg = bg.resize(fg.size)
+    elif fg.size > bg.size:
+        fg = fg.resize(bg.size)
+    bg.alpha_composite(fg)
+    bio = io.BytesIO()
+    bg.save(bio, "PNG")
+    return bio.getvalue()
+
+
+def _extract_drawable(zf: zipfile.ZipFile, infos: Dict[str, zipfile.ZipInfo], drawable: str, *,
+                      resources: Optional[binres.ResourceTableChunk], size: int) -> PIL.Image.Image:
+    if ARGB.fullmatch(drawable):
+        return PIL.Image.new("RGBA", (size, size), _rgba(drawable))
+    if drawable.endswith(".xml"):
+        bio = io.BytesIO(_extract_icon(zf, infos, drawable, resources=resources, size=size))
+    elif drawable.endswith(".png"):
+        bio = io.BytesIO(zf.read(infos[drawable]))
+    else:
+        raise Error(f"Unsupported drawable: {drawable!r}")
+    try:
+        with PIL.Image.open(bio, formats=["PNG"]) as im:
+            im.load()
+            assert isinstance(im, PIL.Image.Image)
+            return im
+    except PIL.UnidentifiedImageError as e:
+        raise Error(f"Unable to load PNG: {e}") from e
+
+
+# FIXME: <shape>? <inset>?
+def _extract_vector(children: Tuple[binres.Chunk, ...], *, size: int) -> bytes:
+    tb = ET.TreeBuilder()
+    vector = None
+    tag_stack: List[binres.XMLElemStartChunk] = []
+    for c in children:
+        if isinstance(c, binres.XMLElemStartChunk):
+            tag_stack.append(c)
+            if c.name == "vector":
+                if vector:
+                    raise Error("Duplicate <vector>")
+                vector = c
+                _convert_vector(tb, c)
+            elif not vector:
+                raise Error("Expected <vector>")
+            elif c.name == "group":
+                _convert_group(tb, c)
+            elif c.name == "path":
+                _convert_path(tb, c)
+            else:
+                raise Error(f"Unsupported tag: {c.name!r}")
+        elif isinstance(c, binres.XMLElemEndChunk):
+            if not tag_stack:
+                raise Error("End tag with empty stack")
+            if c.name != tag_stack.pop().name:
+                raise Error("Expected end tag to match start")
+            tb.end(TAGMAP[c.name])
+            if c.name == "vector":
+                break
+    bio = io.BytesIO()
+    ET.ElementTree(tb.close()).write(bio)
+    data = cairosvg.svg2png(bytestring=bio.getvalue(), output_width=size, output_height=size)
+    assert isinstance(data, bytes)
+    return data
+
+
+# FIXME: tint?
+# FIXME: resources?
+def _convert_vector(tb: ET.TreeBuilder, c: binres.XMLElemStartChunk) -> None:
+    _expect_attrs(c, "width", "height", "viewportWidth", "viewportHeight", "autoMirrored")
+    vpw = c.attr_as_float("viewportWidth", android=True)
+    vph = c.attr_as_float("viewportHeight", android=True)
+    viewbox = f"0 0 {vpw} {vph}"
+    tb.start("svg", {"xmlns": SCHEMA_SVG, "viewBox": viewbox})
+
+
+# FIXME: rotate, ...
+# FIXME: resources?
+def _convert_group(tb: ET.TreeBuilder, c: binres.XMLElemStartChunk) -> None:
+    _expect_attrs(c, "scaleX", "scaleY", "translateX", "translateY")
+    sx = c.attr_as_float("scaleX", android=True, optional=True)
+    sy = c.attr_as_float("scaleY", android=True, optional=True)
+    tx = c.attr_as_float("translateX", android=True, optional=True)
+    ty = c.attr_as_float("translateY", android=True, optional=True)
+    scale_x = str(sx) if sx is not None else "1"
+    scale_y = str(sy) if sy is not None else "1"
+    trans_x = str(tx) if tx is not None else "0"
+    trans_y = str(ty) if ty is not None else "0"
+    transform = f"translate({trans_x}, {trans_y}) scale({scale_x}, {scale_y})"
+    tb.start("g", {"transform": transform})
+
+
+# FIXME: resources?
+def _convert_path(tb: ET.TreeBuilder, c: binres.XMLElemStartChunk) -> None:
+    _expect_attrs(c, "pathData", "fillColor", "strokeColor", "strokeWidth", "strokeAlpha",
+                  "fillAlpha", "strokeLineCap", "strokeLineJoin", "fillType")
+    data = c.attr_as_str("pathData", android=True, optional=True) or ""
+    fill = _colour(c, "fillColor")
+    stroke = _colour(c, "strokeColor")
+    sw = c.attr_as_float("strokeWidth", android=True, optional=True)
+    sa = c.attr_as_float("strokeAlpha", android=True, optional=True)
+    fa = c.attr_as_float("fillAlpha", android=True, optional=True)
+    slc = c.attr_as_int("strokeLineCap", android=True, optional=True)
+    slj = c.attr_as_int("strokeLineJoin", android=True, optional=True)
+    ft = c.attr_as_int("fillType", android=True, optional=True)
+    try:
+        attrs = {
+            "d": data, "fill": fill, "stroke": stroke,
+            "stroke-width": str(sw) if sw is not None else "0",
+            "stroke-opacity": str(sa) if sa is not None else "1",
+            "fill-opacity": str(fa) if fa is not None else "1",
+            "stroke-linecap": CAP[slc if slc is not None else 0],
+            "stroke-linejoin": JOIN[slj if slj is not None else 0],
+            "fill-rule": FILL[ft if ft is not None else 1],
+        }
+    except KeyError as e:
+        raise Error("Unsupported attr value") from e
+    tb.start("path", attrs)
+
+
+def _expect_attrs(c: binres.XMLElemStartChunk, *attrs: str) -> None:
+    expected_attrs = [f"{{{binres.SCHEMA_ANDROID}}}{a}" for a in attrs]
+    for attr in c.attrs_as_dict:
+        if attr not in expected_attrs:
+            raise Error(f"Unsupported attr for <{c.name}>: {attr!r}")
+
+
+# FIXME: resources?
+def _colour(c: binres.XMLElemStartChunk, attr: str) -> str:
+    if not (a := c.attrs_as_dict.get(f"{{{binres.SCHEMA_ANDROID}}}{attr}")):
+        return "none"
+    t, T = a.typed_value.type, binres.BinResVal.Type
+    if t not in (T.INT_COLOR_ARGB8, T.INT_COLOR_RGB8, T.INT_COLOR_ARGB4, T.INT_COLOR_RGB4):
+        raise Error("Expected colour value")
+    colour = binres.brv_str_deref(a.typed_value, a.raw_value)
+    return "none" if colour == "#00000000" else _rgba(colour)
+
+
+def _rgba(colour: str) -> str:
+    return colour if colour == "none" else f"#{colour[3:]}{colour[1:3]}"
+
+
+# vim: set tw=80 sw=4 sts=4 et fdm=marker :
